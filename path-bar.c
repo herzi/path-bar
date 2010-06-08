b16b1c2bf13329448fd26e883f133ed2308557cf
Sven Herzberg <herzi@gnome-de.org>
2010-06-08 16:55:56 +0200

    start to remove the hardcoded elements
    * path-bar.c: remove the hardcoded elements from the expose handler

--- path-bar.c
+++ path-bar.c
@@ -23,31 +23,69 @@
 #define GETTEXT_PACKAGE NULL
 #include <glib/gi18n-lib.h>
 
+typedef struct _ProgressPathBarElement
+{
+  gchar* icon_name;
+  gchar* label;
+} ProgressPathBarElement;
+
+struct _ProgressPathBarPrivate
+{
+  GList* elements;
+};
+
+#define PRIV(i) (((ProgressPathBar*)(i))->_private)
+
 G_DEFINE_TYPE (ProgressPathBar, progress_path_bar, PROGRESS_TYPE_SIMPLE_WIDGET);
+
+static void
+add_element (ProgressPathBar* self,
+             gchar const    * icon_name,
+             gchar const    * label)
+{
+  ProgressPathBarElement* element = g_slice_new (ProgressPathBarElement);
+  element->icon_name = g_strdup (icon_name);
+  element->label     = g_strdup (label);
+
+  PRIV (self)->elements = g_list_append (PRIV (self)->elements, element);
+}
 
 static void
 progress_path_bar_init (ProgressPathBar* self G_GNUC_UNUSED)
 {
+  PRIV (self) = G_TYPE_INSTANCE_GET_PRIVATE (self, PROGRESS_TYPE_PATH_BAR, ProgressPathBarPrivate);
+
+  add_element (self, GTK_STOCK_HOME, NULL);
+  add_element (self, NULL, N_("Programming"));
+  add_element (self, NULL, "GTK+");
+  add_element (self, NULL, N_("Path Bar"));
+}
+
+static void
+free_element (gpointer  data,
+              gpointer  user_data G_GNUC_UNUSED)
+{
+  ProgressPathBarElement* element = data;
+  g_free (element->icon_name);
+  g_free (element->label);
+  g_slice_free (ProgressPathBarElement, element);
+}
+
+static void
+finalize (GObject* object)
+{
+  g_list_foreach (PRIV (object)->elements, free_element, NULL);
+  g_list_free (PRIV (object)->elements);
+
+  G_OBJECT_CLASS (progress_path_bar_parent_class)->finalize (object);
 }
 
 static gboolean
 expose_event (GtkWidget     * widget,
               GdkEventExpose* event)
 {
-  struct
-    {
-      gchar const* icon_name;
-      gchar const* label;
-    }
-  elements[] =
-    {
-        {GTK_STOCK_HOME, NULL},
-        {NULL, N_("Programming")},
-        {NULL, N_("GTK+")},
-        {NULL, N_("Path Bar")}
-    };
   cairo_t* cr = gdk_cairo_create (event->window);
-  guint i;
+  GList  * iter;
 
   gdk_cairo_region (cr, event->region);
   cairo_clip (cr);
@@ -57,10 +95,11 @@ expose_event (GtkWidget     * widget,
   cairo_save (cr);
   cairo_arc (cr, 4.5, 20.5, 4.0, 0.5 * G_PI, G_PI);
   cairo_arc (cr, 4.5, 4.5, 4.0, G_PI, 1.5 * G_PI);
-  for (i = 0; i < G_N_ELEMENTS (elements); i++)
+  for (iter = PRIV (widget)->elements; iter; iter = iter->next)
     {
+      ProgressPathBarElement* element = iter->data;
       double intern = 0.0;
-      if (i > 0)
+      if (iter->prev)
         {
           cairo_new_path (cr);
           cairo_move_to (cr, 0.5, 24.5);
@@ -72,10 +111,10 @@ expose_event (GtkWidget     * widget,
       cairo_translate (cr, 4.0, 0.0);
       intern += 4.0;
 
-      if (elements[i].icon_name)
+      if (element->icon_name)
         {
           /* FIXME: cache the pixbuf after realize() and update on every "style-set" */
-          GdkPixbuf* buf = gtk_widget_render_icon (widget, elements[i].icon_name, GTK_ICON_SIZE_MENU, NULL);
+          GdkPixbuf* buf = gtk_widget_render_icon (widget, element->icon_name, GTK_ICON_SIZE_MENU, NULL);
           cairo_save (cr);
           cairo_translate (cr, 0.0, 4.0);
           gdk_cairo_set_source_pixbuf (cr, buf, 0.0, 0.0);
@@ -85,10 +124,10 @@ expose_event (GtkWidget     * widget,
           intern += gdk_pixbuf_get_width (buf) + 4.0;
           g_object_unref (buf);
         }
-      if (elements[i].label)
+      if (element->label)
         {
           /* FIXME: cache the layout after realize() and update on every "style-set" */
-          PangoLayout* layout = gtk_widget_create_pango_layout (widget, elements[i].label);
+          PangoLayout* layout = gtk_widget_create_pango_layout (widget, element->label);
           PangoRectangle  logical;
           cairo_path_t* path = cairo_copy_path (cr);
 
@@ -108,7 +147,7 @@ expose_event (GtkWidget     * widget,
           g_object_unref (layout);
         }
 
-      if (i < G_N_ELEMENTS (elements) - 1)
+      if (iter->next)
         {
           cairo_pattern_t* pattern = cairo_pattern_create_linear (-intern, 0.0, 0.0, 24.0);
           cairo_pattern_add_color_stop_rgba (pattern, 0.0, 0.0, 0.0, 0.0, 0.15);
@@ -142,9 +181,14 @@ expose_event (GtkWidget     * widget,
 static void
 progress_path_bar_class_init (ProgressPathBarClass* self_class)
 {
+  GObjectClass  * object_class = G_OBJECT_CLASS (self_class);
   GtkWidgetClass* widget_class = GTK_WIDGET_CLASS (self_class);
 
+  object_class->finalize = finalize;
+
   widget_class->expose_event = expose_event;
+
+  g_type_class_add_private (self_class, sizeof (ProgressPathBarPrivate));
 }
 
 GtkWidget*
